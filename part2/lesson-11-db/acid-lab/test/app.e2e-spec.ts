b0d9e982f2d5b6d265ce315a6723d7daa86e3a09
Vitalik <vdemko@exoft.net>
2025-07-23 01:41:38 +0300

    e2e

--- part2/lesson-11-db/acid-lab/test/app.e2e-spec.ts
+++ part2/lesson-11-db/acid-lab/test/app.e2e-spec.ts
@@ -1,25 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { DataSource, Repository } from 'typeorm';
+import { Account } from './../src/transfer/entities/account.entity';
+import { Movement } from './../src/transfer/entities/movement.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('Transfer(e2e)', () => {
+  let app: INestApplication;
+  let dataSource: DataSource;
+  let accountRepo: Repository<Account>;
+  let movementRepo: Repository<Movement>;
 
-  beforeEach(async () => {
+  const fromId = '11111111-1111-1111-1111-111111111111';
+ 
+  const toId = '22222222-2222-2222-2222-222222222222';
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    dataSource = moduleFixture.get(DataSource);
+    accountRepo = moduleFixture.get(getRepositoryToken(Account));
+    movementRepo = moduleFixture.get(getRepositoryToken(Movement));
+
+    // clare db 
+    await movementRepo.createQueryBuilder().delete().execute();
+    await accountRepo.createQueryBuilder().delete().execute();
+
+    //create accounts
+    await accountRepo.insert([
+      { id: fromId, balance: 100 },
+      { id: toId, balance: 200 },
+    ]);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Does not allow transfer when the balance is insufficient, and the database remains unchanged', async () => {
+    await request(app.getHttpServer())
+      .post('/transfer')
+      .send({ fromId, toId, amount: 999 })
+      .expect(400);
+
+    const from = await accountRepo.findOneBy({ id: fromId });
+    const to = await accountRepo.findOneBy({ id: toId });
+    const movements = await movementRepo.find();
+
+    expect(Number(from?.balance)).toBe(100);
+    expect(Number(to?.balance)).toBe(200);
+    expect(movements.length).toBe(0);
   });
 });
